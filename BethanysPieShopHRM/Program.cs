dc91bf65d81f72b1fa14c0c3efd4f4368b1d7e5f
Wenjuan <wenjuan.wang@yahoo.com>
2024-11-25 22:26:46 -0500

    Adding method to calculate yearly wage with overloading

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -3,6 +3,20 @@ using BethanysPieShopHRM;
 
 Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
+int amount = 1234;
+int months = 12;
+int bonus = 1000;
+
+int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
+int yearlyWageWithBonus = Utilities.CalculateYearlyWage(amount, months, bonus);
+
+Console.WriteLine($"Yearly wage: {yearlyWage}");
 
 
-Console.ReadLine();
+double amountDouble = 1500.0;
+double monthsDouble = 12;
+double bonusDouble = 1000;
+
+double yearlyWageWithBonusDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+
+
--- BethanysPieShopHRM/Utilities.cs
+++ BethanysPieShopHRM/Utilities.cs
@@ -13,6 +13,34 @@ namespace BethanysPieShopHRM
 
             return monthlyWage * numberOfMonthsWorked;
         }
+
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus)
+        {
+            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+        }
+
+        public static double CalculateYearlyWage(double monthlyWage, double numberOfMonthsWorked, double bonus)
+        {
+            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+        }
+
+        public static void UsingOptionalParameters()
+        {
+            int monthlyWage1 = 1234;
+            int months1 = 12;
+
+            int yearlyWageForEmployee1 = CalculateYearlyWageWithOptional(monthlyWage1, months1);
+            Console.WriteLine($"Yearly wage for employee 1 (Bethany): {yearlyWageForEmployee1}");
+        }
+
+        public static int CalculateYearlyWageWithOptional(int monthlyWage, int numberOfMonthsWorked, int bonus = 0)
+        {
+
+            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+            return monthlyWage * numberOfMonthsWorked + bonus;
+        }
     }
 }
 
