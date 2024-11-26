48324a278de360aac32a7dd7008ddd476b6b32c7
Wenjuan <wenjuan.wang@yahoo.com>
2024-11-25 22:14:43 -0500

    Adding a Utilities file

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using BethanysPieShopHRM;
 
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+
+
+Console.ReadLine();
---
+++ BethanysPieShopHRM/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BethanysPieShopHRM
+{
+	public class Utilities
+	{
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            //Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked}");
+            //return monthlyWage * numberOfMonthsWorked;
+
+            if (numberOfMonthsWorked == 12)//let's add a bonus month
+                return monthlyWage * (numberOfMonthsWorked + 1);
+
+            return monthlyWage * numberOfMonthsWorked;
+        }
+    }
+}
+
