71a7f880581f01c648ae923d7a09476553e67cf9
Wenjuan <wenjuan.wang@yahoo.com>
2024-11-26 19:03:09 -0500

    Adding helper functions to manipulate strings

--- BethanysPieShopHRM/Utilities.cs
+++ BethanysPieShopHRM/Utilities.cs
@@ -3,6 +3,62 @@ namespace BethanysPieShopHRM
 {
 	public class Utilities
 	{
+        public static void UsingSimpleStrings()
+        {
+
+            string firstName = "Bethany";
+            string lastName = "Smith";
+            string s;
+            s = firstName;
+            var userName = "BethanyS";
+            userName = userName.ToLower();
+
+            userName = string.Empty;
+            userName = "";//identical to string.Empty;
+        }
+
+        public static void ManipulatingStrings()
+        {
+            string firstName = "Bethany";
+            string lastName = "Smith";
+
+
+            string fullName = firstName + " " + lastName;
+            string employeeIdentification = String.Concat(firstName, lastName);
+            //string empId = firstName.ToLower() + "-" + firstName.ToLower();
+            string empId = firstName.ToLower() + "-" + lastName.Trim().ToLower();
+            int length = empId.Length;
+
+
+            if (fullName.Contains("beth") || fullName.Contains("Beth"))
+            {
+                Console.WriteLine("It's Bethany!");
+            }
+            string subString = fullName.Substring(1, 3);
+            Console.WriteLine("Characters 2 to 4 of fullName are " + subString);
+
+            //string interpolation
+            string nameUsingInterpolation = $"{firstName}-{lastName}";
+            Console.WriteLine(nameUsingInterpolation);
+            //combined with implicit typing
+            var v = $"Hello, {firstName}!";
+            Console.WriteLine(v);
+        }
+
+        public static void UsingEscapeCharacters()
+        {
+            string firstName = "Bethany";
+            string lastName = "Smith";
+
+            string displayName = $"Welcome!\n{firstName}\t{lastName}";
+
+            string filePath = "C:\\data\\employeelist.xlsx";
+            string marketingTagLine = "Baking the \"best pies\" ever";
+
+            string verbatimFilePath = @"C:\data\employeelist.xlsx";
+        }
+
+
         public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
         {
             //Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked}");
